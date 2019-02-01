34b7fbaae0e151eaf2a6b94b25da3ea458a8fb1f
ronny <ronny@ixpantia.com>
2019-02-01 14:21:08 -0600

    esqueleto de la funncion para pipeline report

---
+++ R/get_pipeline_report.R
@@ -0,0 +1,18 @@
+#'
+#' @description Return your pipeline report information from Less annoying CRM
+#' @title get_pipeline_report
+#'
+#' @param user_code The user code to identify your account
+#' @param api_token The api token to connect to your account
+#'
+#' @export
+get_account_information <- function(user_code, api_token) {
+  llamado <- paste0("https://api.lessannoyingcrm.com?", "UserCode=", user_code,
+                    "&APIToken=", api_token, "&Function=GetUserInfo")
+  r <- httr::GET(llamado)
+  contenido <- httr::content(r, "text")
+  contenido <- jsonlite::fromJSON(contenido)
+  contenido <- as.data.frame(contenido)
+
+  return(contenido)
+}
