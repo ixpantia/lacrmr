a163f6c71407e54429916fdc2bd0196e9f9bd506
ronny <ronny@ixpantia.com>
2019-02-01 11:32:17 -0600

    prueba con modify_url

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -14,14 +14,23 @@ api_token <- "GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88"
 url <- "https://api.lessannoyingcrm.com"
 funcion <- "GetUserInfo"
 
-get_contacts <- function()
+# TODO// recordar que en los llamados para el api, practicamente lo que cambia
+# es la function del llamado, lo demas se mantendria igual. Se podria crear
+# una funcione que guarde los datos del usuario para que luego solo sea
+# necesario especificar la funcion. O bien, unicamente con el nombre de la
+# funcion ya deberia de ser innecesario especificar el parametro en los
+# argumentos y se podria dejar fijo dentro de la funcion.
+get_contacts <- function(url, user_code, api_token) {
+
+}
+
+modify_url(url, path = user_code)
 
 
 
 
 
-
-$EndpointURL?UserCode=$UserCode&APIToken=$APIToken
+# $EndpointURL?UserCode=$UserCode&APIToken=$APIToken
 
 r <- GET("https://api.lessannoyingcrm.com?UserCode=6EBF0&APIToken=GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88&Function=SearchContacts")
 contenido <- content(r, "text")
