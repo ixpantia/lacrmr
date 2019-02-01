a474c64fd67d38f6a6267f864dca158d85a72350
ronny <ronny@ixpantia.com>
2019-02-01 14:51:53 -0600

    cambios en estructura de funcion para inclusion de parametros
    aun no sirve la inclusion de los parametros

--- R/get_pipeline_report.R
+++ R/get_pipeline_report.R
@@ -1,17 +1,23 @@
 
 #' @title get_pipeline_report
 #'
-#' @description Return your pipeline report information from Less annoying CRM
+#' @description Return your pipeline report information from Less annoying CRM.
+#' For this you will need to know the pipelineId, StatusId, and CustomFieldId
+#' You can get this PipelineId's at https://www.lessannoyingcrm.com/app/Settings/Api
 #'
 #'
 #' @param user_code The user code to identify your account
 #' @param api_token The api token to connect to your account
 #'
 #' @export
-get_account_information <- function(user_code, api_token) {
+get_account_information <- function(user_code, api_token, pipelineId) {
   llamado <- paste0("https://api.lessannoyingcrm.com?", "UserCode=", user_code,
                     "&APIToken=", api_token, "&Function=GetPipelineReport")
-  r <- httr::GET(llamado)
+  r <- httr::GET(llamado,
+                 query = list(
+                   PipelineId = pipelineId
+                 )
+  )
   contenido <- httr::content(r, "text")
   contenido <- jsonlite::fromJSON(contenido)
   contenido <- as.data.frame(contenido)
@@ -19,5 +25,8 @@ get_account_information <- function(user_code, api_token) {
   return(contenido)
 }
 
+pipelineId <- 3571432457735893289690341276529
 
-r <- GET("https://api.lessannoyingcrm.com?UserCode=6EBF0&APIToken=GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88&Function=GetPipelineReport")
+
+# Prueba debe de ser que si no hay un parametro bien identificado el cuadro
+# final va a tener una columna que se llama "succes" y va a contener failure
