6eafefff2da1a0a100339a010e66fb6b906d2de9
ronny <ronny@ixpantia.com>
2019-02-01 13:24:01 -0600

    primera funcion generada
    funcion trae la informacion de los contactos del CRM

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -1,39 +1,31 @@
-@import httr
-@import jsonlite
+#' @import httr
+#'
+#' @description Return the contacts information from Less annoying CRM
+#' @title get_contacts
+#'
+#' @param user_code The user code to identify your account
+#' @param api_token The api token to connect to your account
+#'
+#' @export
+get_contacts <- function(user_code, api_token) {
+  llamado <- paste0("https://api.lessannoyingcrm.com?", "UserCode=", user_code,
+                    "&APIToken=", api_token, "&Function=SearchContacts")
+  r <- GET(llamado)
+  contenido <- content(r, "text")
+  contenido <- fromJSON(contenido)
+  contenido <- as.data.frame(contenido)
+
+  return(contenido)
+}
 
 # Estos son los parametros:
 # $EndpointURL, $UserCode, $APIToken, $Function, $Parameters)
 
-#' Trae datos de contactos del CRM
+# url <- "https://www.lessannoyingcrm.com/app/"
+# user_code <- "6EBF0"
+# api_token <- "GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88"
+# url <- "https://api.lessannoyingcrm.com"
+# funcion <- "GetUserInfo"
 
-#'
+# r <- GET("https://api.lessannoyingcrm.com?UserCode=6EBF0&APIToken=GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88&Function=SearchContacts")
 
-url <- "https://www.lessannoyingcrm.com/app/"
-user_code <- "6EBF0"
-api_token <- "GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88"
-url <- "https://api.lessannoyingcrm.com"
-funcion <- "GetUserInfo"
-
-# TODO// recordar que en los llamados para el api, practicamente lo que cambia
-# es la function del llamado, lo demas se mantendria igual. Se podria crear
-# una funcione que guarde los datos del usuario para que luego solo sea
-# necesario especificar la funcion. O bien, unicamente con el nombre de la
-# funcion ya deberia de ser innecesario especificar el parametro en los
-# argumentos y se podria dejar fijo dentro de la funcion.
-get_contacts <- function(url, user_code, api_token) {
-
-}
-
-modify_url(url, path = user_code)
-
-
-
-
-
-# $EndpointURL?UserCode=$UserCode&APIToken=$APIToken
-
-r <- GET("https://api.lessannoyingcrm.com?UserCode=6EBF0&APIToken=GCQB7XPQNR1Z5BRV2XMHM0PFGCFQJGFZK8MP98SY0TWSCVJK88&Function=SearchContacts")
-contenido <- content(r, "text")
-contenido <- fromJSON(contenido)
-contenido <- as.data.frame(contenido)
-headers(r)
