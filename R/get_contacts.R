9dd9b7b622866c7b5bab880c8afabd03f94bbf51
ronny <ronny@ixpantia.com>
2019-02-04 16:19:18 -0600

    listas como dataframe pero de diferente tamano
    hay que encontrar una solucion mas simple para obtner los datos de las listas y que queden acorde con su respectivo nombre

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -15,16 +15,30 @@ get_contacts <- function(user_code, api_token, contactid) {
     APIToken = api_token,
     Function = 'SearchContacts',
     Parameters = contactid
-  )
+      )
   )
 
   contenido <- httr::content(r, "text")
   contenido <- jsonlite::fromJSON(contenido)
   contenido <- as.data.frame(contenido)
 
-  correo <- contenido %>%
-    filter(!is.na(Result.FirstName)) %>%
-    select(Result.Email)
+  listas <- contenido %>%
+    select(Result.Email, Result.Address, Result.Phone,
+           Result.Website)
+
+  #Esto lo sigue arrastrando como lista pero lo hace parecer tibble
+  # email <- contenido %>%
+  #   select(Result.Email) %>%
+  #   bind_rows()
+
+  email <- do.call(rbind.data.frame, contenido$Result.Email)
+  website <- do.call(rbind.data.frame, contenido$Result.Website)
+  phone <- do.call(rbind.data.frame, contenido$Result.Phone)
+  address <- do.call(rbind.data.frame, contenido$Result.Address)
+
+  #TODO// Pueden salir de diferente tamano, no se va a poder pegar
+  tabla_listas <- data_frame(email, website, phone, address)
+
 
   return(contenido)
 }
