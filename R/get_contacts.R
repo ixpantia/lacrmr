e9dbfcfb0c1b450ddf47ea64b5541953bf720989
ronny <ronny@ixpantia.com>
2019-02-05 12:10:21 -0600

    documentacion intentos por ordenar listas de listas

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -21,6 +21,8 @@ get_contacts <- function(user_code, api_token, contactid) {
   contenido <- httr::content(r, "text")
   contenido <- jsonlite::fromJSON(contenido)
   contenido <- as.data.frame(contenido)
+  contenido <- contenido %>%
+    filter(!is.na(Result.FirstName))
 
   listas <- contenido %>%
     select(Result.Email, Result.Address, Result.Phone,
@@ -39,6 +41,57 @@ get_contacts <- function(user_code, api_token, contactid) {
   #TODO// Pueden salir de diferente tamano, no se va a poder pegar
   tabla_listas <- data_frame(email, website, phone, address)
 
+  daniel$Result.Email[[1]]$Text
+
+  for (i in 1:nrow(contenido)) {
+    print(contenido$Result.Email[[i]]$Text)
+  }
+
+  contenido$Phone <- NA
+  for (i in 1:nrow(contenido)) {
+    print(contenido[contenido$Result.Phone[[i]]$Text == ""]) #<- NA)
+    # contenido$Phone[i] <- (contenido$Result.Phone[[i]]$Text)
+  }
+
+  contenido$Phone <- NA
+  prueba <- list()
+  for (i in 1:nrow(contenido)) {
+    prueba[i] <- (contenido$Result.Phone[[i]]$Text) #<- NA)
+    # contenido$Phone[i] <- (contenido$Result.Phone[[i]]$Text)
+  }
+
+  # Asi me traigo los valores. Resta hacerlos un dataframe
+  #Esto no se puede porque elimina los null y con eso el espacio
+  prueba <- do.call(rbind, lapply(contenido$Result.Email, rbind))
+  prueba_2 <- do.call(rbind, lapply(contenido$Result.Phone, rbind))
+  prueba_3 <- do.call(rbind, lapply(listas, rbind))
+
+  prueba_4 <- qdapTools::list_df2df(listas)
+
+  y$series[[1]]$data <- lapply(y$series[[1]]$data,
+                     function(z) { z[ lengths(z) == 0 ] <- NA; z; })
+
+  nombres <- contenido %>%
+    filter(!is.na(Result.FirstName))
+  # NULL lo puedo diferenciar si tiene length cero:
+  length(nombres$Result.Phone[[2]])
+  # Puedo hacer un condicional que transforme lo de length cero a NA
+
+  a <- length(nombres$Result.Phone[[2]] <- NA)
+
+  #Este es buen condicional:
+  for (i in 1:nrow(nombres)) {
+    print(length(nombres$Result.Phone[[i]]) == 1)
+  }
+
+  # Intento para cambiar a NA
+  for (i in 1:nrow(nombres)) {
+    print(length(nombres$Result.Phone[[i]]$Text) == 0)
+  }
+
+  nombres$Result.Phone[length(nombres$Result.Phone[[i]]) == 0] <- NA
+
+  nombres$Result.Phone[[2]]$Text <- NA
 
   return(contenido)
 }
