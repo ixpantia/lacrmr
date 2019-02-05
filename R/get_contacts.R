82f680a6f2a3714ddd7dffd5913e9651e30a0a53
ronny <ronny@ixpantia.com>
2019-02-05 12:26:03 -0600

    limpieza script
    todos los intentos y anotaciones para limpiar listas se han eliminado

--- R/get_contacts.R
+++ R/get_contacts.R
@@ -24,73 +24,16 @@ get_contacts <- function(user_code, api_token, contactid) {
   contenido <- contenido %>%
     filter(!is.na(Result.FirstName))
 
-  listas <- contenido %>%
-    select(Result.Email, Result.Address, Result.Phone,
-           Result.Website)
-
-  #Esto lo sigue arrastrando como lista pero lo hace parecer tibble
-  # email <- contenido %>%
-  #   select(Result.Email) %>%
-  #   bind_rows()
-
-  email <- do.call(rbind.data.frame, contenido$Result.Email)
-  website <- do.call(rbind.data.frame, contenido$Result.Website)
-  phone <- do.call(rbind.data.frame, contenido$Result.Phone)
-  address <- do.call(rbind.data.frame, contenido$Result.Address)
-
-  #TODO// Pueden salir de diferente tamano, no se va a poder pegar
-  tabla_listas <- data_frame(email, website, phone, address)
-
-  daniel$Result.Email[[1]]$Text
-
-  for (i in 1:nrow(contenido)) {
-    print(contenido$Result.Email[[i]]$Text)
-  }
-
-  contenido$Phone <- NA
-  for (i in 1:nrow(contenido)) {
-    print(contenido[contenido$Result.Phone[[i]]$Text == ""]) #<- NA)
-    # contenido$Phone[i] <- (contenido$Result.Phone[[i]]$Text)
-  }
-
-  contenido$Phone <- NA
-  prueba <- list()
-  for (i in 1:nrow(contenido)) {
-    prueba[i] <- (contenido$Result.Phone[[i]]$Text) #<- NA)
-    # contenido$Phone[i] <- (contenido$Result.Phone[[i]]$Text)
-  }
-
-  # Asi me traigo los valores. Resta hacerlos un dataframe
-  #Esto no se puede porque elimina los null y con eso el espacio
-  prueba <- do.call(rbind, lapply(contenido$Result.Email, rbind))
-  prueba_2 <- do.call(rbind, lapply(contenido$Result.Phone, rbind))
-  prueba_3 <- do.call(rbind, lapply(listas, rbind))
-
-  prueba_4 <- qdapTools::list_df2df(listas)
-
-  y$series[[1]]$data <- lapply(y$series[[1]]$data,
-                     function(z) { z[ lengths(z) == 0 ] <- NA; z; })
-
   nombres <- contenido %>%
     filter(!is.na(Result.FirstName))
-  # NULL lo puedo diferenciar si tiene length cero:
-  length(nombres$Result.Phone[[2]])
-  # Puedo hacer un condicional que transforme lo de length cero a NA
 
-  a <- length(nombres$Result.Phone[[2]] <- NA)
-
-  #Este es buen condicional:
-  for (i in 1:nrow(nombres)) {
-    print(length(nombres$Result.Phone[[i]]) == 1)
-  }
-
-  # Intento para cambiar a NA
+  # Intento para cambiar a NA en data.frame
   for (i in 1:nrow(nombres)) {
     nombres$Result.Phone[i][(length(nombres$Result.Phone[[i]]$Text) == 0)] <- list(data.frame("Text" = NA, "Type" = NA, "Clean" = NA))
   }
 
 
-2}
+}
 
 # Estos son los parametros:
 # $EndpointURL, $UserCode, $APIToken, $Function, $Parameters)
