ebf696aa3e0240448683389e6c92d4ba893c5cd6
ronny <ronny@ixpantia.com>
2019-05-02 18:23:53 -0600

    Ref #20 cambois para pruebas

--- R/get_contact_information.R
+++ R/get_contact_information.R
@@ -16,36 +16,36 @@ get_contact_information <- function(user_code, api_token, contact_id = "") {
   } else if (missing(api_token)) {
     warning("Please add a valid API token")
   } else
-    tryCatch(
-      {
-        lacrm_url <- "https://api.lessannoyingcrm.com"
+    tryCatch({
+      lacrm_url <- "https://api.lessannoyingcrm.com"
 
-        r <- httr::GET(lacrm_url, query = list(
-          UserCode = user_code,
-          APIToken = api_token,
-          Function = 'GetContact',
-          Parameters = paste0('{"ContactId":','"', contact_id, '"', '}')
-        )
-        )
+      r <- httr::GET(lacrm_url, query = list(
+        UserCode = user_code,
+        APIToken = api_token,
+        Function = 'GetContact',
+        Parameters = paste0('{"ContactId":','"', contact_id, '"', '}')
+      )
+      )
 
-        contenido <- httr::content(r, "text")
-        contenido <- jsonlite::fromJSON(contenido,
-                                        simplifyVector = TRUE)
+      contenido <- httr::content(r, "text")
+      contenido <- jsonlite::fromJSON(contenido,
+                                      simplifyVector = TRUE)
 
-        for (i in 1:length(contenido$Contact)){
-          # print(contenido[["Contact"]][[i]])
-          contenido$Contact[i][(is.null(contenido$Contact[[i]]) == TRUE)] <- NA
-          contenido$Contact[i][(sjmisc::is_empty(contenido$Contact[[i]]) == TRUE)] <- NA
-          contenido$Contact[i][(contenido$Contact[[i]] == "")] <- NA
-        }
-
-        contenido <- as.data.frame(contenido) %>%
-          dplyr::select(-Success) %>%
-          janitor::clean_names()
-
-        return(contenido)
+      for (i in 1:length(contenido$Contact)){
+        # print(contenido[["Contact"]][[i]])
+        contenido$Contact[i][(is.null(contenido$Contact[[i]]) == TRUE)] <- NA
+        contenido$Contact[i][(sjmisc::is_empty(contenido$Contact[[i]]) == TRUE)] <- NA
+        contenido$Contact[i][(contenido$Contact[[i]] == "")] <- NA
       }
-    )
+
+      contenido <- as.data.frame(contenido) %>%
+        dplyr::select(-Success) %>%
+        janitor::clean_names()
+
+      return(contenido)
+    })
 
 }
 
+
+#TODO: HACER FUNCION INTERNA QUE TRAIGA JSON PARA PRUEBA
--- tests/testthat/test-limpiar.R
+++ tests/testthat/test-limpiar.R
@@ -1,15 +1,21 @@
 context("limpieza")
 
+check_api <- function() {
+  if (not_working()) {
+    skip_if_offline(host = "r-project.org")
+  }
+}
+Sys.setenv(pipelineid = "3504965832666975645900460114637")
+
 testthat::test_that("listas son eliminadas de datos de contactos", {
-  # TODO// como sin hacer conexion puedo probar la funcion
+  check_api()
   api_token <- Sys.getenv("api_token")
-  user_code <- Sys.getenv("user_code")
-  pipelineid = Sys.getenv("pipelineid")
+  user_code <- Sys.getenv("CODIGO_USUARIO")
+  pipelineid <- Sys.getenv("pipelineid")
 
   limpieza <- get_pipeline_report(api_token = api_token,
                                   user_code = user_code,
                                   pipelineid = pipelineid)
-
 
 })
 
