fd07da7af0e2425c5df2a30033b54000312d81b7
ronny <ronny@ixpantia.com>
2020-02-22 15:56:08 -0600

    Ref #1 estructura en pruebas

--- tests/testthat/test-credentials.R
+++ tests/testthat/test-credentials.R
@@ -18,6 +18,18 @@ testthat::test_that("Account information error message with invalid credentials"
 })
 
 
+testthat::test_that("Contact information error message with invalid credentials", {
+  codigo_usuario <- "6A6E88"
+  token_api <- "7HTQ9XM8VXPDG0XV0KMVFBHV7CCB"
+
+  expect_error(get_account_information(user_code = codigo_usuario,
+                                       api_token = token_api),
+               "Invalid user credentials. Please check your user code or your api token")
+})
+
+
+
+
 
 
 
--- tests/testthat/test-limpiar.R
+++ tests/testthat/test-limpiar.R
@@ -3,10 +3,7 @@ context("limpieza")
 testthat::test_that("listas son eliminadas de datos de contactos", {
 
   datos <- jsonlite::fromJSON(contact_information)
-  limpieza <- get_test_contact_information(datos)
-  expect_true(class(limpieza) != "list")
-  expect_true(ncol(limpieza) == 24)
-  expect_true(nrow(limpieza) == 1)
+
 
 })
 
