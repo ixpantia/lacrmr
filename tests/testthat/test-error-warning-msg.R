f9c64449ac0ad36f4753e341cadb5e77cf92e9df
ronny <ronny@ixpantia.com>
2020-04-09 14:33:34 -0600

    Ref #22 error check in test

--- tests/testthat/test-error-warning-msg.R
+++ tests/testthat/test-error-warning-msg.R
@@ -7,10 +7,10 @@ testthat::test_that("Invalid credentials", {
     "Please add a valid user code"
   )
 
-  expect_warning(
-    get_pipeline_report(user_code = "6A6E88",
-                        pipelineid = "57102821"),
-    "Please add a valid API token"
+  expect_error(
+    get_account_information(user_code = "6A6E88",
+                            api_token = "57102821"),
+    "Invalid user credentials. Please check your user code or your api token"
   )
 
   expect_warning(
