97d9a8c99b758586b2e4b1e25884acd2f467ab62
ronny <ronny@ixpantia.com>
2020-04-09 13:46:28 -0600

    Ref #28 global variables

--- R/lacrmr.R
+++ R/lacrmr.R
@@ -10,4 +10,10 @@
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-utils::globalVariables(c(".", "Success"))
+utils::globalVariables(c(".", "Success", "result_email", "result_phone",
+                         "result_address", "result_website",
+                         "result_contact_custom_fields", "Result.FirstName",
+                         "result_email", "result_phone", "result_website",
+                         "filter", "select", "Text", "Type", "bind_cols",
+                         "result_custom_fields"
+                         ))
